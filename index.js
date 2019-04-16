d7100569e2ff2472161b05c69633da69a458b774
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-16 17:27:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(words){
+  return words.toUpperCase
+}
+
+function whisper(words){
+  return words.toLowerCase
+}
+
+function logShout(words){
+  console.log(words.toUpperCase)
+}
+
+function logWhisper(words){
+  console.log(words.toLowerCase);
+}
+
+function sayHiToGrandma(words){
+
+}
